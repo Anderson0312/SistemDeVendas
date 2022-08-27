08fc91f017307460534bc5886b012f508bfbaf96
Anderson0312 <andersonmoura812@gmail.com>
2022-08-26 21:41:48 -0300

    att

--- src/formularios/frmPrincipal.java
+++ src/formularios/frmPrincipal.java
@@ -401,7 +401,7 @@ public class frmPrincipal extends javax.swing.JFrame {
         panelPizza.add(btnPizza5);
 
         panelPrincipla.add(panelPizza);
-        panelPizza.setBounds(6, 6, 467, 567);
+        panelPizza.setBounds(6, 6, 900, 650);
 
         dpnDesk.setLayer(jPanel1, javax.swing.JLayeredPane.DEFAULT_LAYER);
         dpnDesk.setLayer(jPanel2, javax.swing.JLayeredPane.DEFAULT_LAYER);
@@ -414,7 +414,7 @@ public class frmPrincipal extends javax.swing.JFrame {
             .addGroup(dpnDeskLayout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(panelPrincipla, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(panelPrincipla, javax.swing.GroupLayout.DEFAULT_SIZE, 556, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
